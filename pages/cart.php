23efcf2b6086f4bb84132cbc0f5dddbf690f32c7
fakhr <fakhr.eldin1984@gmail.com>
2025-01-07 18:29:13 +0200

    First Commit Only Upload The Project

---
+++ pages/cart.php
@@ -0,0 +1,79 @@
+<?php
+$title = "Cards";
+/* --- Head --- */
+require_once('inc/head.php');
+/* --- Nav --- */
+require_once('inc/nav.php');
+/* --- Header --- */
+require_once('inc/header.php');
+?>
+<!-- Section-->
+<section class="py-5">
+    <div class="container px-4 px-lg-5 mt-5">
+        <div class="row">
+            <div class="col-12">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Product</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">Quantity</th>
+                            <th scope="col">Total</th>
+                            <th scope="col">Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Product 1</td>
+                            <td>$9.99</td>
+                            <td>
+                                <input type="number" value="1">
+                            </td>
+                            <td>$9.99</td>
+                            <td>
+                                <a href="#" class="btn btn-danger">Delete</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Product 2</td>
+                            <td>$19.99</td>
+                            <td>
+                                <input type="number" value="2">
+                            </td>
+                            <td>$9.99</td>
+                            <td>
+                                <a href="#" class="btn btn-danger">Delete</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Product 2</td>
+                            <td>$19.99</td>
+                            <td>
+                                <input type="number" value="2">
+                            </td>
+                            <td>$9.99</td>
+                            <td>
+                                <a href="#" class="btn btn-danger">Delete</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">
+                                Total Price
+                            </td>
+                            <td colspan="3">
+                                <h3>3325 $</h3>
+                            </td>
+                            <td>
+                                <a href="checkout.php" class="btn btn-primary">Checkout</a>
+                            </td>
+                        </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+</section>
+<?php require_once('inc/footer.php'); ?>
---
+++ pages/contact.php
@@ -0,0 +1,38 @@
+<?php
+$title = "Contact";
+/* --- Head --- */
+require_once('inc/head.php');
+/* --- Nav --- */
+require_once('inc/nav.php');
+/* --- Header --- */
+require_once('inc/header.php');
+?>
+<!-- Section-->
+<section class="py-5">
+    <div class="container px-4 px-lg-5 mt-5">
+        <div class="row">
+            <div class="col-8 mx-auto">
+                <form action="" class="form border my-2 p-3">
+                    <div class="mb-3">
+                        <div class="mb-3">
+                            <label for="">Name</label>
+                            <input type="text" name="" id="" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <label for="">Email</label>
+                            <input type="email" name="" id="" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <label for="">Message</label>
+                            <textarea name="" id="" class="form-control" rows="7"></textarea>
+                        </div>
+                        <div class="mb-3">
+                            <input type="submit" value="Send" id="" class="btn btn-success">
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+<?php require_once('inc/footer.php'); ?>
