83a2373ebdbabefd9fb891c99d76c4deab001a4e
fakhr <fakhr.eldin@ymail.com>
2025-01-10 21:55:23 +0200

    master

---
+++ about.php
@@ -0,0 +1,51 @@
+<?php
+$title = "About";
+require_once('header.php'); ?>
+<?php require_once('nav.php'); ?>
+?>
+<!-- Header-->
+<header class="bg-dark py-5">
+    <div class="container px-4 px-lg-5 my-5">
+        <div class="text-center text-white">
+            <h1 class="display-4 fw-bolder">Shop in style</h1>
+            <p class="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
+        </div>
+    </div>
+</header>
+<?php require_once('nav.php'); ?>
+<!-- Section-->
+<section class="py-5">
+    <div class="container px-4 px-lg-5 mt-5">
+        <div class="row">
+            <div class="col-12">
+                <div class="border p-4 text-center my-5">
+                    <h2>Our Mission</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi asperiores sit rem sint commodi quibusdam facere error totam deleniti officiis.
+                    </p>
+                </div>
+
+                <div class="border p-4 text-center my-5">
+                    <h2>Our Vission</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi asperiores sit rem sint commodi quibusdam facere error totam deleniti officiis.
+                    </p>
+                </div>
+
+                <div class="border p-4  my-5">
+                    <h2 class="text-center">Our Goals</h2>
+                    <h6 class="border p-3 my-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, dolore.</h6>
+                    <h6 class="border p-3 my-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, dolore.</h6>
+                    <h6 class="border p-3 my-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, dolore.</h6>
+                    <h6 class="border p-3 my-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, dolore.</h6>
+                    <h6 class="border p-3 my-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, dolore.</h6>
+                    <h6 class="border p-3 my-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, dolore.</h6>
+                    <h6 class="border p-3 my-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, dolore.</h6>
+                    <h6 class="border p-3 my-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, dolore.</h6>
+                    <h6 class="border p-3 my-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, dolore.</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php require_once('footer.php'); ?>
---
+++ checkout.php
@@ -0,0 +1,77 @@
+<?php
+$title = "Checkout";
+require_once('header.php'); ?>
+<?php require_once('nav.php'); ?>
+?>
+
+<!-- Header-->
+<header class="bg-dark py-5">
+    <div class="container px-4 px-lg-5 my-5">
+        <div class="text-center text-white">
+            <h1 class="display-4 fw-bolder">Shop in style</h1>
+            <p class="lead fw-normal text-white-50 mb-0">With this shop home page template</p>
+        </div>
+    </div>
+</header>
+<?php require_once('nav.php'); ?>
+<!-- Section-->
+<section class="py-5">
+    <div class="container px-4 px-lg-5 mt-5">
+        <div class="row">
+            <div class="col-4">
+                <div class="border p-2">
+                    <div class="products">
+                        <ul class="list-unstyled">
+                            <li class="border p-2 my-1"> Product #1 -
+                                <span class="text-success mx-2 mr-auto bold">2 x 25$</span>
+                            </li>
+                            <li class="border p-2 my-1"> Product #1 -
+                                <span class="text-success mx-2 mr-auto bold">2 x 25$</span>
+                            </li>
+                            <li class="border p-2 my-1"> Product #1 -
+                                <span class="text-success mx-2 mr-auto bold">2 x 25$</span>
+                            </li>
+                            <li class="border p-2 my-1"> Product #1 -
+                                <span class="text-success mx-2 mr-auto bold">2 x 25$</span>
+                            </li>
+                            <li class="border p-2 my-1"> Product #1 -
+                                <span class="text-success mx-2 mr-auto bold">2 x 25$</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <h3>Total : 644 $</h3>
+                </div>
+            </div>
+            <div class="col-8">
+                <form action="" class="form border my-2 p-3">
+                    <div class="mb-3">
+                        <div class="mb-3">
+                            <label for="">Name</label>
+                            <input type="text" name="" id="" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <label for="">Email</label>
+                            <input type="email" name="" id="" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <label for="">Address</label>
+                            <input type="text" name="" id="" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <label for="">Phone</label>
+                            <input type="number" name="" id="" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <label for="">Notes</label>
+                            <input type="text" name="" id="" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <input type="submit" value="Send" id="" class="btn btn-success">
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+<?php require_once('footer.php'); ?>
